f0a47755cf9330ba4c26da51ba32f05cf22c6b32
PrinjayGlobaliya <prinjaymandani@gmail.com>
2025-03-31 23:09:14 +0530

    -created_separate_config_of_the_setting -created_model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django_ckeditor_5.fields import CKEditor5Field
+from star_ratings.models import Rating
+from django.utils.text import slugify
+
+
+
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='posts')
+    description = CKEditor5Field()
+    image = models.ImageField(upload_to='story_images/', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    ratings = models.ForeignKey(Rating, on_delete=models.CASCADE, null=True, blank=True)
+    tags = TaggableManager()
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
---
+++ core/settings/prod.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
