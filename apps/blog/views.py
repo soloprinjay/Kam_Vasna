3670720f2566a5f7755263f14a25bb279581fe76
Soloprinjay <prinjaymandani@gmail.com>
2025-04-12 23:58:31 +0530

    -removed_story_model

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,28 +1,18 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Story
+from .models import Post
 from django.http import Http404
 
 
 class HomeView(View):
     def get(self, request):
         stories = Story.objects.order_by('?')[:3]
-        return render(request, 'home.html', {'stories': stories})        
+        return render(request, 'home.html', {'stories': stories})
 
 class StoriesView(View):
     def get(self, request):
-        category = request.GET.get('category')
-        if category:
-            stories = Story.objects.filter(category=category)
-        else:
-            stories = Story.objects.all()
-
-        categories = [c[0] for c in Story.CATEGORY_CHOICES]
-        return render(request, 'stories.html', {
-            'stories': stories,
-            'categories': categories,
-            'selected_category': category
-        })
+        stories = Story.objects.all()
+        return render(request, 'stories.html', {'stories': stories})        
 
 class StoryDetailView(View):
     def get(self, request, slug):
