93ecb28e656b8bc7291e724fa2998d5aca2b4ac9
Soloprinjay <prinjaymandani@gmail.com>
2025-04-13 22:33:11 +0530

    -Changed_title_words -Created_contact_post_method

--- core/settings/base.py
+++ core/settings/base.py
@@ -108,6 +108,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
