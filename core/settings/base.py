f992ba582436b68f16898525bbc6bc1f72d8dca3
Soloprinjay <prinjaymandani@gmail.com>
2025-04-19 17:25:40 +0530

    -added_user_model

--- core/settings/base.py
+++ core/settings/base.py
@@ -221,3 +221,5 @@ CKEDITOR_5_CONFIGS = {
 
 # Define a constant in settings.py to specify file upload permissions
 CKEDITOR_5_FILE_UPLOAD_PERMISSION = "staff"  # Possible values: "staff", "authenticated", "any"
+
+AUTH_USER_MODEL = "users.CustomUser"
